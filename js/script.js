643593dcd623533c9a0c5816d72782af709bd125
JIN <t19887348@hotmail.com>
2021-09-10 10:06:55 +0800

    login Modal & facebook login API

--- js/script.js
+++ js/script.js
@@ -1,23 +1,85 @@
-// navigation Bar transform
+//Facebook login API ---------------
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '550144832925914',
+    cookie: true,
+    xfbml: true,
+    version: 'v11.0'
+  })
+  FB.AppEvents.logPageView()
+}
+;(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) {
+    return
+  }
+  js = d.createElement(s)
+  js.id = id
+  js.src = 'https://connect.facebook.net/en_US/sdk.js'
+  fjs.parentNode.insertBefore(js, fjs)
+})(document, 'script', 'facebook-jssdk')
+
+const fbLogin = document.querySelector('#facebook-btn')
+
+fbLogin.onclick = function () {
+  FB.getLoginStatus(function (response) {
+    if (response.authResponse) {
+      FB.api('/me', { fields: 'id,name,email' })
+    } else {
+      FB.login(
+        function (response) {
+          if (response.authResponse) {
+            FB.api('/me', { fields: 'id,name,email' })
+          }
+        },
+        { scope: 'email' }
+      )
+    }
+  })
+}
+
+// navigation Bar transform ----------------------
 const bar = document.querySelector('.bar-container')
 const nav = document.querySelector('nav')
 const bodyContainer = document.querySelector('.body-container')
-let i = 1
+let menu = 1
 
 bar.addEventListener('click', function () {
   this.classList.toggle('change')
-  if (i === 1) {
+  if (menu === 1) {
     nav.style.transform = 'translateX(0)'
     bodyContainer.style.transform = 'translateX(30%)'
-    i = 0
+    menu = 0
   } else {
     nav.style.transform = 'translateX(-200%)'
     bodyContainer.style.transform = 'translateX(0)'
-    i = 1
+    menu = 1
   }
 })
 
-//Slider
+//Login ----------------------
+const mask = document.querySelector('.mask')
+const login = document.querySelector('.login-btn')
+const loginContainer = document.querySelector('.login-container')
+const loginClose = document.querySelector('.close')
+
+login.addEventListener('click', function () {
+  mask.style.display = 'block'
+  loginContainer.style.display = 'block'
+})
+
+mask.onclick = function () {
+  loginContainer.style.display = 'none'
+  this.style.display = 'none'
+}
+
+loginClose.onclick = function () {
+  loginContainer.style.display = 'none'
+  mask.style.display = 'none'
+}
+
+//Slider ----------------------
 const swiper = new Swiper('.mySwiper', {
   autoplay: {
     delay: 2000,
